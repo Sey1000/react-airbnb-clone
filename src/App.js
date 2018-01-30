5e2c8e3f902372b32db11c3d7aaab02e6ba39a88
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2018-01-30 23:08:28 +0100

    Implement google-map-react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GoogleMapReact from 'google-map-react';
 import './App.css';
 import Flat from "./components/flat";
 
@@ -22,6 +22,11 @@ class App extends Component {
   }
 
   render() {
+    const center = {
+      lat: 48.8566,
+      lng: 2.3522
+    };
+
     return (
       <div className="app">
         <div className="main">
@@ -34,6 +39,11 @@ class App extends Component {
           </div>
         </div>
         <div className="map">
+          <GoogleMapReact
+            center={center}
+            zoom={11}
+          >
+          </GoogleMapReact>
         </div>
       </div>
   );
